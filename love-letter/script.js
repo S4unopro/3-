12c5d5648d5d52c7db0f94f4cd8887ecaa565539
S4unopro <s4unopro@gmail.com>
2026-07-06 23:47:58 +0700

    script.js

--- love-letter/script.js
+++ love-letter/script.js
@@ -1,1 +1,10 @@
+const title = document.querySelector("h1");
+const text = document.querySelector("p");
 
+setTimeout(() => {
+    title.textContent = "Happy Anniversary ";
+}, 1000);
+
+setTimeout(() => {
+    text.textContent = "Together since 07/07/2023\nI love you forever ";
+}, 2500);
